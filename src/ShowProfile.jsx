399a516afe78f1d433bdfb557a7a0bab3e8c06cd
officerkiruha <kirillkovalchukstudy@gmail.com>
2026-04-16 20:01:24 +0300

    fixing bug. 5

--- src/ShowProfile.jsx
+++ src/ShowProfile.jsx
@@ -1,8 +1,11 @@
+import profileImg from './Me.jpeg';
+
 export default function ShowProfile() {
     return (
         <div className="card">
             <h1>About Me</h1>
-            <img src="/img/Me.jpeg" alt="My Profile" className="avatar" />
+            {/* 2. מכניסים את המשתנה של התמונה לתוך תגית ה-src עם סוגריים מסולסלים */}
+            <img src={profileImg} alt="My Profile" className="avatar" />
             <p className="muted">
                 Hi, I'm Kirill, 28 years old, living in Giv'at Avni.
                 I serve as a reserve officer in the IDF Ordnance Corps, specializing in technology, maintenance, and operational systems.
